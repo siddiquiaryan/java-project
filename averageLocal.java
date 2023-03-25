f245c63dc4152205de559d1b2fa9955fbffa1325
siddiquiaryan <115453426+siddiquiaryan@users.noreply.github.com>
2023-03-25 15:13:02 +0530

    Add files via upload

---
+++ averageLocal.java
@@ -0,0 +1,12 @@
+public class averageLocal { // average of numbers using Local variables
+    public static void main(String args[])
+    {
+       double num1=10;
+       double num2=36;
+       double num3=77;
+       double num4=57;
+       double num5=(num1+num2+num3+num4)/4;
+       System.out.println("Average="+num5);
+    }
+    
+}
