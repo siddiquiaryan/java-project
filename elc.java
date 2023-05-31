fdea1cb2a1bb821651ecf2a93db67472b2919212
siddiquiaryan <115453426+siddiquiaryan@users.noreply.github.com>
2023-05-31 19:13:53 +0530

    Add files via upload

---
+++ elc.java
@@ -0,0 +1,12 @@
+package testing.constructur;
+
+public class elc {
+
+	public static void main(String[] args) {
+		blc d1 = new blc();
+		System.out.println(d1);
+		blc d2 = new blc(22731,"Siddiqui");
+		System.out.println(d2);
+	}
+
+}
