de252992d514c2fdd737726a98d9ec47c0ef9558
siddiquiaryan <115453426+siddiquiaryan@users.noreply.github.com>
2023-05-31 19:18:49 +0530

    Create multilvl_inheritence.java

---
+++ multilvl_inheritence.java
@@ -0,0 +1,24 @@
+public class inheritance {
+    int x= 25;
+    System.out.println(x);  
+
+}
+
+class child extends inheritance{
+    int y=30;
+    System.out.println(x);    
+}
+
+class grandchild extends child{
+    
+    System.out.println(y);
+}
+class Main{
+    public static void main(String args[])
+    {
+        child c1  =                n e                                                                    
+    grandchild g1 = new grandchild();
+
+    }
+
+}
